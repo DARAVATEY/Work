e973563a87ef276ec7ff5564c8aeb94af19a60c1
DARAVATEY <n.daravatey02@gmail.com>
2025-12-20 05:06:06 +0700

    Update geminiService.ts

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -1,4 +1,4 @@
-
+import { GoogleGenerativeAI } from "@google/generative-ai";
 import { GoogleGenAI, Type } from "@google/genai";
 
 export const verifyIdentityMatch = async (base64Image: string, userName: string) => {
